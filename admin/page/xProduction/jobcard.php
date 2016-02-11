ebb4477b0c9c4ff0c52f25f384fa52a7c6dd76c6
anshusushil <milkysushil1920@gmail.com>
2016-02-11 11:31:46 +0530

    xProduction module files created

---
+++ admin/page/xProduction/jobcard.php
@@ -0,0 +1,14 @@
+<?php
+
+class page_xProduction_jobcard extends Page {
+
+    public $title='jobcard';
+
+    function init() {
+        parent::init();
+
+    }
+    function defaultTemplate(){
+		return ['jobcard'];
+
+}
---
+++ admin/page/xProduction/outsourceprofile.php
@@ -0,0 +1,15 @@
+<?php
+
+class page_xProduction_outsourceprofile extends Page {
+
+    public $title='jobcard';
+
+    function init() {
+        parent::init();
+
+    }
+    function defaultTemplate(){
+		return ['outsourceprofile'];
+
+}
+}
