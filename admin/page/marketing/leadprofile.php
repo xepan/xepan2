0ce71812036ce01191a18b7cbbbefd0730bd4d6a
Vaibhav Sharma <vaibhavsharma38@gmail.com>
2016-02-12 11:06:39 +0530

    lead profile (activity) view added

--- admin/page/marketing/leadprofile.php
+++ admin/page/marketing/leadprofile.php
@@ -5,7 +5,8 @@ class page_marketing_leadprofile extends Page{
 	function init(){
 		parent::init();
 
-		$this->add('View_Info')->set('hello');
+		$this->add('View_Marketing_activity',null,'activity');
+
 	}
 
 	function defaultTemplate(){
