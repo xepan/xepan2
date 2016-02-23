1b89be41742a2903609c05f31018d4bf0c0b2ebe
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-23 11:08:00 +0530

    Few pages removed from admin (must be  in add-on application)

--- admin/page/hr/departments.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class page_hr_departments extends Page{
-		function init(){
-			parent::init();
-		}
-		function defaultTemplate(){
-			return ['page/hr/departments'];
-
-	}		}
--- admin/page/hr/post.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class page_hr_post extends Page{
-		function init(){
-			parent::init();
-		}
-		function defaultTemplate(){
-			return ['page/hr/post'];
-		}
-	}
