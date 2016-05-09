e15e491309c7013c085f401f99dba7380584920a
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-05-09 09:36:23 +0530

    make super user super again

--- admin/config-default.php
+++ admin/config-default.php
@@ -10,4 +10,4 @@ $config['tmail']['transport'] = 'Echo';
 // $config['js']['versions']['jquery']='';
 
 $config['developer_mode'] = true;
-$config['all_rights_to_superuser'] = false;
+$config['all_rights_to_superuser'] = true;
