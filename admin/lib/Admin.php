d99190cca0603178a07d12aad0ea80cc07a2fdef
vijay mali <guddavijay@gmail.com>
2016-05-26 11:39:51 +0530

    admin default website name www

--- admin/lib/Admin.php
+++ admin/lib/Admin.php
@@ -43,7 +43,7 @@ class Admin extends App_Frontend {
     function defaultTemplate(){
 
         $url = "{$_SERVER['HTTP_HOST']}";
-        $sub_domain = $this->extract_subdomains($url)?:'default';
+        $sub_domain = $this->extract_subdomains($url)?:'www';
 
         $current_website = $this->current_website_name = $sub_domain;
         $this->readConfig("websites/$this->current_website_name/config.php");
