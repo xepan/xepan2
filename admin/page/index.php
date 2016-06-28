7033591c2dc1fc267be02cc2d1459ccadf110949
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-06-28 10:36:29 +0530

    Added wizard in breadcrumb

--- admin/page/index.php
+++ admin/page/index.php
@@ -4,6 +4,11 @@ class page_index extends \xepan\base\Page {
 
     public $title='Dashboard';
 
+    public $breadcrumb=[
+						'Dashboard'=>'/',
+						'Wizard' => 'xepan_base_wizard'
+					];
+
     function init() {
         parent::init();
         
