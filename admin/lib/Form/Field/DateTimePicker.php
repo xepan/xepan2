88428fbe1f502e6d156121550236640b7c60d165
vaibhav <vaibhavsharma38@gmail.com>
2016-07-19 10:27:12 +0530

    form field datetimepicker added

---
+++ admin/lib/Form/Field/DateTimePicker.php
@@ -0,0 +1,48 @@
+<?php
+
+class Form_Field_DateTimePicker extends Form_Field_Line {
+    public $options=array();
+    function init(){
+        parent::init();
+
+        $this->addCalendarIcon();
+    }
+    function addCalendarIcon() {
+        $this->addButton('',array('options'=>array('text'=>false)))
+            ->setHtml('')
+            ->setIcon(' fa fa-calendar')
+            ->js('click',$this->js()->appendDtpicker('show'));
+        $this->js('focus', $this->js()->appendDtpicker('show'));
+    }
+    function getInput($attr=array()){
+        $this->js(true)->_load('bootstrap-datetimepicker')->_css('libs/bootstrap-datetimepicker');
+        $this->js(true)->appendDtpicker(array_merge(array(
+                    'closeOnSelected'=> true,
+                    'futureOnly'=> true
+                    ),$this->options));
+
+        return parent::getInput(array_merge(
+                    array(
+                        'value'=>'',
+                         ),$attr
+                    ));
+    }
+    function set($value){
+        // value can be valid date format, as in config['locale']['date']
+        if(!$value)return parent::set(null);
+        if(is_int($value)){
+            return parent::set(date('Y-m-d H-i-s',$value));
+        }
+        @list($d,$m,$y)=explode('/',$value);
+        if($y)$value=join('/',array($m,$d,$y));
+        elseif($m)$value=join('/',array($m,$d));
+        $value=date('Y-m-d H-i-s',strtotime($value));
+        return parent::set($value);
+    }
+    function get(){
+        $value=parent::get();
+        // date cannot be empty string
+        if($value=='')return null;
+        return $value;
+    }
+}
