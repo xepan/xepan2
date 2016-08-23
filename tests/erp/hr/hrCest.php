5fbb5b08859d227f69b2ebb1546921450748d07e
vijay mali <guddavijay@gmail.com>
2016-08-23 19:35:55 +0530

    hr test case worknig

--- tests/erp/hr/hrCest.php
+++ tests/erp/hr/hrCest.php
@@ -135,24 +135,37 @@ class hrCest
         // $i->wait(2);
         // $i->click(['css'=>' button.ui-dialog-titlebar-close']);
         // $i->see('Post Employees');
-        $i->wait(2);
-        $i->click(['css'=>'table tbody tr td:nth-child(6) button']);
+
     }
     function test_add_post(SuperUser $i){
         $i->login('management@xavoc.com');
         $i->clickMenu('HR->Post');
-        $i->waitPageLoad();
-        $i->see('Add Post');
+        // $i->waitPageLoad();
+        $i->waitForText('Add Post');
         $i->click('Add Post');
 
         $i->fillAtkField('name','management');
         $i->click('Add');
         $i->waitForElement('.field-error-text');
         $i->see('Department_id must not be empty');
-        $i->wait(10);
         $i->select2Option("department_id",['text'=>'Company']);
         $i->click('Add');
         $i->see('management');
+        $i->click(['css'=>'table tbody tr:nth-child(2) td:nth-child(6) button']);
+        $i->waitForText('Deactivate');
+        $i->click(['css'=>'table tbody tr:nth-child(2) td:nth-child(6) a']);
+        $i->waitPageLoad();
+        $i->dontSee('management');
+        $i->click('InActive');
+        $i->click(['css'=>'table tbody tr:nth-child(2) td:nth-child(6) button']);
+        $i->cancelPopup();
+        $i->cancelPopup();
+        $i->waitForText('Active');
+        $i->click(['css'=>'table tbody tr:nth-child td:nth-child(6) a']);
+        $i->see('No matching records found');
+        $i->click('Active');
+        $i->see('management');
+        $i->wait(5);
     }
     function testEmployee(SuperUser $i){
         $i->login('management@xavoc.com');
@@ -162,16 +175,43 @@ class hrCest
         $i->click('Official');
         $i->waitForText('Offer Date');
         $i->click('Activity');
-        // $i->waitForText("deactivated ");
+        $i->wait(2);
         $i->click('Documents');
         $i->waitForText('No document found');
+        $i->click(['css'=>' button.ui-dialog-titlebar-close']);
     }
+    function test_add_employee(SuperUser $i){
+        $i->login('management@xavoc.com');
+        $i->clickMenu('HR->Employee');
+        $i->see('Add Employee');
+        $i->click('Add Employee');
+        $i->waitPageLoad();
+        $i->fillAtkField('first_name','vijay');
+        $i->fillAtkField('last_name','mali');
+        $i->fillAtkField('organization','Xavoc Technocrats Pvt. Ltd');
+        $i->fillAtkField('address','18/436, Gaytri marg Kanji Ka Hata');
+        $i->wait(10);
+        // $i->selectOption('country', 'India');
+        // $i->selectOption('state', ['Rajasthan']);
+        $i->fillAtkField('city','Udaipur');
+        $i->fillAtkField('pin_code','313001');
+        $i->click('Save');
+    }
+
+    // function test_add_employee_details(SuperUser $i){
+    //     $i->login('management@xavoc.com');
+    //     $i->clickMenu('HR->Employee');
+    //     $i->click(['css'=>'table tbody tr:nth-child(2) td:nth-child(5) a']);
+    //     $i->waitPageLoad();
+    //     $i->select2Option("department_id",['text'=>'Company']);
+    //     // $i->select2Option("post_id",['text'=>'management']);
+    //     $i->save();
+    // }
     
     function testEmployeeMovement(SuperUser $i){
         $i->login('management@xavoc.com');
         $i->clickMenu('HR->Employee Movement');
-        // $i->waitPageLoad();
-        $i->see('Employee Movement');
+        $i->waitForText('Employee Movement');
         
     }
     function testUser(SuperUser $i){
@@ -179,12 +219,117 @@ class hrCest
         $i->clickMenu('HR->User');
         // $i->waitPageLoad();
         $i->see('Add User');
+        $i->click('Add User');
+        $i->waitPageLoad();
+        $i->fillAtkField('username','');
+        $i->click('Add');
+        $i->waitForText('Username must not be empty',2);
+        $i->fillAtkField('username','1231');
+        $i->click('Add');
+        $i->waitForText('Username must be a valid email address',2);
+        
+        $i->fillAtkField('username','vijay.mali552@gmail.com');
+        $i->fillAtkField('password','123');
+        $i->select2Option('scope',['text'=>'SuperUser']);
+        $i->wait(3);
+        $i->click('Add');
     }
+
     function testAffiliate(SuperUser $i){
         $i->login('management@xavoc.com');
         $i->clickMenu('HR->Affiliate');
         // $i->waitPageLoad();
         $i->see('Add Affiliate');
+    }
+
+    function test_add_affiliate(SuperUser $i){
+        $i->login('management@xavoc.com');
+        $i->clickMenu('HR->Affiliate');
+        $i->see('Add Affiliate');
+        $i->click('Add Affiliate');
+        $i->waitPageLoad();
+        $i->fillAtkField('first_name','vijay');
+        $i->fillAtkField('last_name','mali');
+        $i->fillAtkField('organization','Xavoc Technocrats Pvt. Ltd');
+        $i->fillAtkField('post','Developer');
+        $i->fillAtkField('address','18/436, Gaytri marg Kanji Ka Hata');
+        $i->fillAtkField('city','Udaipur');
+        $i->fillAtkField('pin_code','313001');
+        $i->click('Save');
+    }
+
+    function test_add_affiliate_detail(SuperUser $i){
+        $i->login('management@xavoc.com');
+        $i->clickMenu('HR->Affiliate');
+        $i->see('Add Affiliate');
+        $i->click(['css'=>'table tbody tr td:nth-child(4) a.pb_edit']);
+        $i->waitPageLoad();
+        $i->waitForText('Affiliate Details');
+        $i->fillAtkField('narration','Test Case Affiliate');
+        $i->click('Save');
+    }
+    
+    function test_add_affiliate_contact_email(SuperUser $i){
+        $i->login('management@xavoc.com');
+        $i->clickMenu('HR->Affiliate');
+        $i->see('Add Affiliate');
+        $i->click(['css'=>'table tbody tr td:nth-child(4) a.pb_edit']);
+        $i->waitPageLoad();
+        $i->waitForText('Affiliate Details');
+        $i->click('Add Contact Email');
+        $i->waitForText('Adding new Contact Email');
+        $i->select2Option('head','');
+        $i->click('Add');
+        $i->waitForText('Head must not be empty');
+        $i->select2Option('head',['text'=>'Official']);
+        $i->fillAtkField('value','');
+        $i->click('Add');
+        $i->waitForText('Value must not be empty');
+        $i->fillAtkField('value','vijay.mali552@gmail.com');
+        // $i->click('is Active');
+        // $i->click('is Valid');
+        $i->click('Add');
+        $i->waitForText('vijay.mali552@gmail.com');
+    }
+
+    function test_add_affiliate_contact_number(SuperUser $i){
+        $i->login('management@xavoc.com');
+        $i->clickMenu('HR->Affiliate');
+        $i->see('Add Affiliate');
+        $i->click(['css'=>'table tbody tr td:nth-child(4) a.pb_edit']);
+        $i->waitPageLoad();
+        $i->waitForText('Affiliate Details');
+        $i->click('Add Contact Phone');
+        $i->waitForText('Adding new Contact Phone');
+        $i->select2Option('head','');
+        $i->click('Add');
+        $i->waitForText('Head must not be empty');
+        $i->select2Option('head',['text'=>'Mobile']);
+        $i->fillAtkField('value','9784954128');
+        // $i->click('is Active');
+        // $i->click('is Valid');
+        $i->click('Add');
+        $i->waitForText('9784954128');
+    }
+
+    function test_add_affiliate_contact_number(SuperUser $i){
+        $i->login('management@xavoc.com');
+        $i->clickMenu('HR->Affiliate');
+        $i->see('Add Affiliate');
+        $i->click(['css'=>'table tbody tr td:nth-child(4) a.pb_edit']);
+        $i->waitPageLoad();
+        $i->waitForText('Affiliate Details');
+        $i->click('Add Contact IM');
+        $i->waitForText('Adding new Contact IM');
+        $i->select2Option('head','');
+        $i->click('Add');
+        $i->waitForText('Head must not be empty');
+        $i->select2Option('head',['text'=>'WhatsApp']);
+        $i->fillAtkField('value','9784954128');
+        // $i->click('is Active');
+        // $i->click('is Valid');
+        $i->click('Add');
+        $i->waitForText('9784954128');
     }
 
 
