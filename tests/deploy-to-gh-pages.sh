0bdb7326ae59e0473922db3e5ac580477b163ed8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-25 19:13:04 +0530

    removed gitignore file from _output

--- tests/deploy-to-gh-pages.sh
+++ tests/deploy-to-gh-pages.sh
@@ -9,6 +9,6 @@ git config --global user.name "Gowrav Vishwakarma"
 
 
 git init
-git add .
+git add -A .
 git commit -m "Travis Report"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
