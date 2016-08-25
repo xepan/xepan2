4729b712449ed507dab9baca912e040ce3582a62
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-25 18:59:55 +0530

    bug fixed

--- tests/deploy-to-gh-pages.sh
+++ tests/deploy-to-gh-pages.sh
@@ -11,4 +11,4 @@ git config --global user.name "Gowrav Vishwakarma"
 git init
 git add .
 git commit -m "Travis Report"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
