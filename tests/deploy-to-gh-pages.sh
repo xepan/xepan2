ff54e4f1a0676a2d69f155c490d35cd1d1e08c48
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-25 18:03:16 +0530

    publish reports to gh-pages

---
+++ tests/deploy-to-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 
+
+cd tests/_output
+
+# config
+git config --global user.email "gowravvishwakarma@gmail.com"
+git config --global user.name "Gowrav Vishwakarma"
+
+
+git init
+git add .
+git commit -m "Travis Report"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
