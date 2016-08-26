71fb5f07751d1e9cd1e97a3d15c5bd938c1d0ddc
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-26 16:24:10 +0530

    travis env variable bug

--- tests/deploy-to-gh-pages.sh
+++ tests/deploy-to-gh-pages.sh
@@ -7,7 +7,7 @@ cd tests/_output
 git config --global user.email "gowravvishwakarma@gmail.com"
 git config --global user.name "Gowrav Vishwakarma"
 
-mkdir ${GITHUB_REPO}
+mkdir "${GITHUB_REPO}"
 mv !("${GITHUB_REPO}") "${GITHUB_REPO}"
 
 git init
