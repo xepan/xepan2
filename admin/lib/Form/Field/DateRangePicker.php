e481cb9cbb68b0e605faa3d8374696f5baaea3fd
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-10-22 17:37:36 +0530

    Implemented DateRange Picker

--- admin/lib/Form/Field/DateRangePicker.php
+++ admin/lib/Form/Field/DateRangePicker.php
@@ -1,10 +1,37 @@
 <?php
 
+/**
+
+How to use
+    
+    add Field 
+    $fld = $f->addField('DateRangePicker','DateRangePicker')->set($_GET['range']?:'01-01-2016 - 31-12-2016');
+    NOTE: dd-mm-yyyy format to set date with ' - ' as separator. SPACE DASH SPACE
+
+    To read
+
+    if(Form->isSubmitted()){
+        $f['DateRangePicker'] returns as string as given
+        or use
+        $fld->getStartDate() in 'yyyy-mm-dd'
+        $fld->getEndDate() in 'yyyy-mm-dd'
+        in form submit
+    }
+    
+*/
+
 class Form_Field_DateRangePicker extends Form_Field_Line {
     public $options=array();
+    
+    public $start_date=null;
+    public $end_date =null;
+
     function init(){
         parent::init();
-        $this->addCalendarIcon();
+        // if(!$this->start_date) $this->start_date = $this->app->today;
+        // if(!$this->end_date) $this->end_date = $this->app->today;
+        
+        // $this->addCalendarIcon();
 
     }
     function addCalendarIcon() {
@@ -21,8 +48,11 @@ class Form_Field_DateRangePicker extends Form_Field_Line {
         ;
 
         $this->js(true,"$('#".$this->name."').daterangepicker({
-                            'startDate': 'start',
-                            'endDate': 'end',
+                            locale: {
+                                  format: 'DD-MM-YYYY'
+                            },
+                            'startDate': '".$this->start_date."',
+                            'endDate': '".$this->end_date."',
                             'ranges': {
                                'Today': [moment(), moment()],
                                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
@@ -35,23 +65,48 @@ class Form_Field_DateRangePicker extends Form_Field_Line {
 
         return parent::getInput(array_merge(
                     array(
-                        'value'=>date('Y-m-d H-i-s',strtotime($this->value)),
+                        'value'=>$this->value,
                          ),$attr
                     ));
     }
+
+    // function setStartDate($date){
+    //     $this->js(true)->daterangepicker('setStartDate',$date);
+    // }
+
+    // function setEndDate($date){
+    //     $this->js(true)->daterangepicker('setEndDate',$date);
+    // }
+
     function set($value){
         // value can be valid date format, as in config['locale']['date']
         if(!$value)return parent::set(null);
-        if(is_int($value)){
-            return parent::set(date('Y-m-d H-i-s',$value));
-        }
-        @list($d,$m,$y)=explode('/',$value);
-        if($y)$value=join('/',array($m,$d,$y));
-        elseif($m)$value=join('/',array($m,$d));
-        $value=date('Y-m-d H-i-s',strtotime($value));
+        // if(is_int($value)){
+        //     return parent::set(date('Y-m-d H-i-s',$value));
+        // }
+        // @list($d,$m,$y)=explode('/',$value);
+        // if($y)$value=join('/',array($m,$d,$y));
+        // elseif($m)$value=join('/',array($m,$d));
+        // $value=date('Y-m-d H-i-s',strtotime($value));
+        $dates = explode(" - ", $value);
+        $this->start_date = date('d-m-Y',strtotime($dates[0]));
+        $this->end_date = date('d-m-Y',strtotime($dates[1]));
+        // var_dump($this->start_date, $this->end_date);
+        // throw new \Exception('set Called' .$value, 1);
         return parent::set($value);
     }
-    function get(){
+
+    function getStartDate(){
+        $dates = explode(" - ", $this->value);
+        return date('Y-m-d',strtotime($dates[0]));
+    }
+
+    function getEndDate(){
+        $dates = explode(" - ", $this->value);
+        return date('Y-m-d',strtotime($dates[1]));   
+    }
+
+    function get(){        
         $value=parent::get();
         // date cannot be empty string
         if($value=='')return null;
