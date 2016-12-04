90df6faed4aaf8f20546be7a8d390cd920038fa8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-12-04 22:12:21 +0530

    working base model for AD

--- admin/lib/AD.php
+++ admin/lib/AD.php
@@ -2,20 +2,18 @@
 
 class AD extends \atk4\data\Model {
 
+    public $_default_class_addField = 'atk4\data\Field_SQL';
+
 	public function __construct($persistence = null, $defaults = [])
     {
         
         parent::__construct($persistence,$defaults);
         $this->app = $GLOBALS['api'];
         $this->persistence = $this->app->ad_db;
-        $this->short_name = uniqid();
-        $this->default_controller = null;
-        $this->caption=null;
-        $this->strict_field_check=false;
-		$this->persistence->app->ad_db->add($this);
     }
 
     function init(){
         parent::init();
+        
 	}
 }
