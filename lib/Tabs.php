5763e85e09b1867879bb9b8e84e8c0a3db53a723
sinharakesh <rksinha.btech@gmail.com>
2017-11-05 12:55:29 +0530

    tabs updated

--- lib/Tabs.php
+++ lib/Tabs.php
@@ -6,8 +6,12 @@ class Tabs extends View_Tabs_jUItabs {
 	function addTab($title,$name=null){
         $container=$this->add('View_HtmlElement',$name);
 
+        $url = '#'.$container->name;
+        if($_GET['cut_page'])
+            $url = $this->app->url('.').'#'.$container->name;
+
         $this->tab_template->set(array(
-                    'url'=>$this->app->url('.').'#'.$container->name,
+                    'url'=>$url,
                     'tab_name'=>$title,
                     'tab_id'=>$container->short_name,
                     ));
