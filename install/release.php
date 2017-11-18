af5b00a3fa49e3ff672f4488353f4af8631ea837
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-11-18 12:48:12 +0530

    added release file

---
+++ install/release.php
@@ -0,0 +1,92 @@
+<?php
+
+set_time_limit(0);
+
+if(file_exists('../atk4')){
+	unlink('../atk4');
+}
+
+
+if(file_exists('../admin/atk4')){
+	unlink('../admin/atk4');
+}
+
+if(file_exists('../admin/vendor')){
+	unlink('../admin/vendor');
+}
+
+if(file_exists('../admin/websites')){
+	unlink('../admin/websites');
+}
+
+if(file_exists('../admin/xepantemplates')){
+	unlink('../admin/xepantemplates');
+}
+
+
+if(file_exists('atk4')){
+	unlink('atk4');
+}
+
+if(file_exists('vendor')){
+	unlink('vendor');
+}
+
+if(file_exists('websites')){
+	unlink('websites');
+}
+
+if(file_exists('xepantemplates')){
+	unlink('xepantemplates');
+}
+
+
+if(file_exists('../websites')){
+	rrmdir('../websites');
+	mkdir('../websites');
+	file_put_contents('../websites/404.html','404');
+}
+
+chdir('..');
+echo 'In Dir <b>'. getcwd() .'</b><br/>';
+echo 'Pulling origin master <br/>';
+$output= shell_exec('git pull origin master');
+echo "output:<br/> <pre>$output</pre>";
+
+$apps = ['accounts','base','blog','cms','commerce','communication','crm','hr','marketing','production','projects'];
+
+$root=getcwd();
+foreach ($apps as $app) {
+	chdir($root);
+	chdir('vendor/xepan/'.$app);
+
+	echo 'In Dir <b>'. getcwd() .'</b><br/>';
+	echo 'Checking out <br/>';
+	$output= shell_exec('git checkout origin master');
+	echo "output:<br/> <pre>$output</pre>";
+
+	echo 'Pulling origin master <br/>';
+	$output=shell_exec('git pull origin master');
+	echo "output:<br/> <pre>$output</pre>";
+}
+
+chdir($root);
+
+$zip_cmd= "zip -r xepan2.zip . --exclude *.svn* --exclude *.git* --exclude *.DS_Store*";
+echo '<b>'.$zip_cmd.'</b><br/>';
+$output = shell_exec($zip_cmd);
+echo "output:<br/> <pre>$output</pre>";
+
+
+function rrmdir($dir) {
+   if (is_dir($dir)) {
+     $objects = scandir($dir);
+     foreach ($objects as $object) {
+       if ($object != "." && $object != "..") {
+         if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+       }
+     }
+     reset($objects);
+     rmdir($dir);
+   }
+} 
