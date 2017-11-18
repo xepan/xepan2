93d3e492a4825b019461e6794278b489fbefb188
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-11-18 14:13:35 +0530

    remove epanservices before release

--- install/release.php
+++ install/release.php
@@ -92,6 +92,19 @@ foreach ($apps as $app) {
 
 chdir($root);
 
+// remove epanservices
+rrmdir('vendor/xepan/epanservices/lib/Controller');
+rrmdir('vendor/xepan/epanservices/lib/Tool');
+rrmdir('vendor/xepan/epanservices/lib/View');
+rrmdir('vendor/xepan/epanservices/page');
+rrmdir('vendor/xepan/epanservices/templates');
+unlink('vendor/xepan/epanservices/lib/Model/Agency.php');
+unlink('vendor/xepan/epanservices/lib/Model/ChannelPartner.php');
+unlink('vendor/xepan/epanservices/lib/Model/DomainDetails.php');
+unlink('vendor/xepan/epanservices/lib/Model/Epan.php');
+unlink('vendor/xepan/epanservices/lib/Model/myTemplates.php');
+
+
 $version="";
 if($_GET['v']){
 	$version='-'.$_GET['v'];
