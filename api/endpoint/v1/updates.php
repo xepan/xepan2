77447152507168af902c7f66bcb3c15fe0d43470
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-11-20 21:38:35 +0530

    better message for api

--- api/endpoint/v1/updates.php
+++ api/endpoint/v1/updates.php
@@ -5,7 +5,7 @@ class endpoint_v1_updates extends Endpoint_REST {
 
 	function get(){
 		
-		$updates = "no updates";
+		$updates = "This is a functional Beta, We are working on better UX, please submit your queries to <a href='http://epan.in/forum' target='_epanforum'>Forum</a>";
 		echo $updates;
 		exit;
 	}
