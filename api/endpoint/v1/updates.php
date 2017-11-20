56292255bc5e7d9313657d0ac0ce75b9c919e2f6
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-11-20 21:41:35 +0530

    read updatex.txt file if exists

--- api/endpoint/v1/updates.php
+++ api/endpoint/v1/updates.php
@@ -6,6 +6,9 @@ class endpoint_v1_updates extends Endpoint_REST {
 	function get(){
 		
 		$updates = "This is a functional Beta, We are working on better UX, please submit your queries to <a href='http://epan.in/forum' target='_epanforum'>Forum</a>";
+		if(file_exists('updates.txt')){
+			$updates=file_get_contents('updates.txt');
+		}
 		echo $updates;
 		exit;
 	}
