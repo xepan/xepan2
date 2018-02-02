1ca321c1c4401061db3832fd11c678aa90d41ca0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-02-02 13:09:07 +0530

    updated xepan/atk4.3 also as some nesty bugs are removed

--- install/release.php
+++ install/release.php
@@ -95,6 +95,10 @@ foreach ($apps as $app) {
 	echo "output:<br/> <pre>$output</pre>";
 }
 
+echo "Updating xepan/atk 4.3<br/>";
+$output=shell_exec('git pull origin 4.3');
+echo "output:<br/> <pre>$output</pre>";
+
 chdir($root);
 
 // remove epanservices
