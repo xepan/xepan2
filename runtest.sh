5e2406883c907d5d051745600f66b90037e3818f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-12 23:13:39 +0530

    run test in local in diffrent environment

--- runtest.sh
+++ runtest.sh
@@ -8,4 +8,5 @@ rm -rf tests/functional;
 rm -rf tests/unit;
 echo Copying tests from $1;
 cp -r vendor/xepan/$1/tests/* tests
-codecept run ${2:-acceptance} --steps --html
+echo "codecept run ${2:-acceptance} --env local_mac --steps --html"
+codecept run ${2:-acceptance} --env local_mac --steps --html
