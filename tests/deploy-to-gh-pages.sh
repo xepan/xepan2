270a5211f70abac5e16f2a19b1eb49b736052ff8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-12 14:12:59 +0530

    overwrite files to keep al existing outputs of other branches

--- tests/deploy-to-gh-pages.sh
+++ tests/deploy-to-gh-pages.sh
@@ -11,11 +11,13 @@ mkdir ~/temp
 mv * ~/temp
 
 mkdir "${REPORT_FOLDER}"
-mv ~/temp/* ./"${REPORT_FOLDER}"
 
 git init
 git remote add origin https://github.com/xavoctechnocratspvtltd/_xepan2.git
 git pull origin gh-pages
+
+mv ~/temp/* ./"${REPORT_FOLDER}"
+
 git add -A .
 git commit -m "Travis Report"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
