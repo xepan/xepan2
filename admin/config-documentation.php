6f52be303c3ae2d013e114c39a3b408d88551913
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-28 11:04:11 +0530

    initial guideline for documenation on any page

--- admin/config-documentation.php
+++ admin/config-documentation.php
@@ -1,5 +1,15 @@
 <?php
 
 $config['documentation']=[
-	'xepan_hr_post'=>['title'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0']
+	'xepan_hr_post'=>[
+					'title'=>'HO/POST',
+					'desc'=>'Manage your company\'s posts here on this page',
+					'blocks'=>[
+						'About HR'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
+						'About Post'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
+						'Best Practices'=>['file'=>'vendor/xepan/hr/templates/documentation/hr.html'],
+						'Content'=>['url'=>'http://documentation.xavoc.com']
+					]
+					
+				]
 ];
