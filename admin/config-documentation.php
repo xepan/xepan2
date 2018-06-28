3fc6afd03510523449a04a391dae698ca5588d55
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-28 11:57:44 +0530

    updated multi blocks system

--- admin/config-documentation.php
+++ admin/config-documentation.php
@@ -1,29 +1,36 @@
 <?php
 
-// $config['documentation']=[
-// 	'xepan_hr_post'=>[
-// 					'title'=>'HO/POST',
+// 'xepan_hr_post/page'=>[
+// 					'title'=>'HR/POST',
 // 					'desc'=>'Manage your company\'s posts here on this page',
 // 					'blocks'=>[
-// 						'About HR'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
-// 						'About Post'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
-// 						'Best Practices'=>['file'=>'vendor/xepan/hr/templates/documentation/hr.html'],
-// 						'Content'=>['url'=>'http://documentation.xavoc.com']
+// 						'Documentation'=>[
+// 							'About HR'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
+// 							'About Post'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
+// 							'Best Practices'=>['file'=>'vendor/xepan/hr/templates/documentation/hr.html'],
+// 							'Content'=>['url'=>'http://documentation.xavoc.com?cut_page=1']
+// 						],
+// 						'Related Configurations'=>[
+// 							'HR Base'=>['xec_page'=>'xepan_hr_department']
+// 						]
 // 					]
 					
 // 				]
-// ];
-
 
 $config['documentation']=[
 	'xepan_hr_post'=>[
-					'title'=>'HO/POST',
+					'title'=>'HR/POST',
 					'desc'=>'Manage your company\'s posts here on this page',
 					'blocks'=>[
-						'About HR'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
-						'About Post'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
-						'Best Practices'=>['file'=>'vendor/xepan/hr/templates/documentation/hr.html'],
-						'Content'=>['url'=>'http://documentation.xavoc.com']
+						'Documentation'=>[
+							'About HR'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
+							'About Post'=>['video'=>'https://www.youtube.com/embed/5KOJsme6Vb4?rel=0'],
+							'Best Practices'=>['file'=>'vendor/xepan/hr/templates/documentation/hr.html'],
+							'Content'=>['url'=>'http://documentation.xavoc.com?cut_page=1']
+						],
+						'Related Configurations'=>[
+							'HR Base'=>['xec_page'=>'xepan_hr_department']
+						]
 					]
 					
 				]
