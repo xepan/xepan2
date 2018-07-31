fd5ec8bf57e47fbec687239cb662f917b39abe63
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-07-31 15:11:37 +0530

    404 page now can be overridable

--- page/404.php
+++ page/404.php
@@ -15,7 +15,16 @@ class page_404 extends \Page {
 	function init(){
 		parent::init();
 		header('HTTP/1.0 404 Not Found');
-		echo "404- Page not found";
-		exit();
+	}
+
+	function defaultTemplate(){		
+		if(file_exists(getcwd().'/websites/'.$this->app->current_website_name.'/www/404.html')){			
+			return ['404'];
+		}
+		else{
+			echo "404- Page not found";
+			exit();
+			return parent::defaultTemplate();
+		}
 	}
 }
